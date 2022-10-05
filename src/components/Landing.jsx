114957f5cda015386c2994e898ca4e80dcfecc94
Bailey <bjonescarew1@gmail.com>
2022-10-05 14:30:51 +1100

    Init commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import UndrawBooks from '../assets/Undraw_Books.svg'
+
+export default function Landing() {
+  return (
+    <section id='landing'>
+        <header>
+            <div className="header__container">
+                <div className="header__description">
+                    <h1>Australia's most awarded online library platform</h1>
+                    <h2>Find your dream book with <span className="purple">Library</span></h2>
+                    <Link to="#features">
+                        <button className="btn">Browse books</button>
+                    </Link>
+                </div>
+                <figure className='header__img--wrapper'>
+                    <img src={UndrawBooks} alt="" />
+                </figure>
+            </div>
+        </header>
+    </section>
+  )
+}
---
+++ src/pages/Bookinfo.jsx
@@ -0,0 +1,93 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Book from "../components/ui/Book";
+import Price from "../components/ui/Price";
+import Rating from "../components/ui/Rating";
+
+export default function Bookinfo({ books, addToCart, cart }) {
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find((book) => book.id === +id);
+  }
+
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/book" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img src={book.url} alt="" className="book__selected--img" />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
+                <div className="book__selected--price">
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
+                </div>
+                <div className="book__summary">
+                  <h3 className="book__summary--title">Summary</h3>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus impedit adipisci esse, voluptas nesciunt, maxime
+                    nulla in labore eum amet autem repellendus itaque earum,
+                    blanditiis unde odio iure sint neque.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloribus impedit adipisci esse, voluptas nesciunt, maxime
+                    nulla in labore eum amet autem repellendus itaque earum,
+                    blanditiis unde odio iure sint neque.
+                  </p>
+                </div>
+                {bookExistsOnCart() ? (
+                  <Link to={`/cart`} className="book__link">
+                    <button className="btn">Checkout</button>
+                  </Link>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
+                    Add to cart
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
